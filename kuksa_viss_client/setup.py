a486742038279ef1df7a4ada6cc1e3ba639d0641
Romain Létendart (BEG/ENG1-FR) <romain.letendart@fr.bosch.com>
2022-09-27 14:21:23 +0200

    setup: Move setuptools configurations to config files
    - setup_requires is deprecated thus moving its content into pyproject.toml's "[build-system] requires".
    - Only keep setuptools-git-versioning's options where values are different from default ones.

--- kuksa_viss_client/setup.py
+++ kuksa_viss_client/setup.py
@@ -10,18 +10,7 @@ class BuildCommand(distutils.command.build.build):
 
 
 setuptools.setup(
-    setuptools_git_versioning={
-        "template": "{tag}",
-        "dev_template": "{tag}-{ccount}",
-        "dirty_template": "{tag}-{ccount}",
-        "starting_version": "0.1.6",
-        "version_callback": None,
-        "version_file": None,
-        "count_commits_from_version_file": False
-    },
-    package_data={ "kuksa_viss_client": ["logo"], "kuksa_certificates": ["*", "jwt/*"]},
-    setup_requires=['setuptools-git-versioning', 'grpcio', 'grpcio-tools'],
     cmdclass={
-    'build': BuildCommand,
+        'build': BuildCommand,
     }
 )
